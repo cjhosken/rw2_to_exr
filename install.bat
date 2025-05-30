cae8af413db0c021f58e0476d21fbc3b7c729e76
Christopher Hosken <hoskenchristopher@gmail.com>
2025-05-30 18:53:21 +0000

    base bones app

---
+++ install.bat
@@ -0,0 +1,10 @@
+
+set "SCRIPT_DIR=%~dp0"
+cd %SCRIPT_DIR%
+python -m venv .venv
+
+call .\.venv\Scripts\activate.bat
+
+python -m pip install -r .\requirements.txt 
+
+python -m PyInstaller --windowed --onefile ./rw2toexr.py
